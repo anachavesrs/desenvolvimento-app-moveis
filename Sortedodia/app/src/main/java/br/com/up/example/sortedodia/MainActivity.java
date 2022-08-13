e1c39773060393b9c4febe91e437c57e1f9cae03
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-08-12 21:32:55 -0300

    commit

--- Sortedodia/app/src/main/java/br/com/up/example/sortedodia/MainActivity.java
+++ Sortedodia/app/src/main/java/br/com/up/example/sortedodia/MainActivity.java
@@ -3,12 +3,19 @@ package br.com.up.example.sortedodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewTitle;
+    private Button buttonAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewTitle = findViewById(R.id.text_title);
+        buttonAction = findViewById(R.id.button_action);
     }
 }
